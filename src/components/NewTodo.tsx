e4279e92f81921eb0833b010e28f8c62d0ef1dd9
Alan Yang <chingyy@g.clemson.edu>
2024-04-15 15:33:54 -0400

    Add React.FormEvent

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,12 @@
+const NewTodo = () => {	
+	const submitHandler = (event: React.FormEvent) => {
+			event.preventDefault();
+	};
+	return <form onSubmit={submitHandler}>
+		<label htmlFor="text">Todo Text</label>
+		<input type="text" id="text" />
+		<button>Add Todo</button>
+	</form>
+};
+
+export default NewTodo;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ToDo from "../models/todo";
+import TodoItem from "./ToDoItem";
 const Todos: React.FC<{items: ToDo[]}> = (props) => {
 	return (
 		<ul>
-			{props.items.map((item) => <li key={item.id}>{item.text}</li>)}
+			{props.items.map((item) => (
+				<TodoItem key={item.id} text={item.text} />
+			))}
 		</ul>
 	)
 }
